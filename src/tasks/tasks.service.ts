c99f071706753e15d6cb539bb42deebf3050a4f3
= <glezgalvan.adrian@gmail.com>
2024-03-18 10:04:15 +0000

    Creating task schema and mongodb conections to tasksService

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from 'src/schemas/tasks.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  findAll() {
+    this.taskModel.find()
+  }
+
+  async create(task: any) {
+    const createdTask = new this.taskModel(task)
+    await createdTask.save()
+    return createdTask
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id)
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id)
+  }
+
+  async update(id: string, task: any) {
+    return this.taskModel.findByIdAndUpdate(id, task)
+  }
+}
